dd9495282951b3ba3a79f4f479dfa82047c9fddd
SadineniAbhi <abhisadineni@gmail.com>
2023-10-08 12:07:58 +0530

    added a route

--- hellopuppy/basic.py
+++ hellopuppy/basic.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "<h1>hello Puppy!!</h1>"
 
+@app.route("/information")
+def info():
+    return "<h1>puppies are cute</h1>"
+
 if __name__ == "__main__":
     app.run()
